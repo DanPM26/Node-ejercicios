e76c07f8f0770ccd288c24bf90236b49d2cfca0c
dany_ponce <siponce.dfwc5@ucamp.utel.edu.mx>
2022-04-25 15:39:33 -0500

    Primer comit

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('#refresh');
+
+const buscarFrase = async() => {
+    const res = await fetch("https://api.chucknorris.io/jokes/random");
+    console.log(res)
+    const frase = await res.json();
+    console.log(frase.value);
+
+    document.querySelector('#text-content').innerHTML= frase.value;
+  };
+  
+
+  btn.addEventListener('click',() => {
+      document.querySelector('#text-content').innerHTML = '';
+      buscarFrase();
+  });
+
+  buscarFrase();
