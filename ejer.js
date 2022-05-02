667a6b11b37b53e4215c458548a938d8f9e38e9a
DanPM26 <98349420+DanPM26@users.noreply.github.com>
2022-05-02 15:08:19 -0500

    Add files via upload
    Calculadora usando node

---
+++ ejer.js
@@ -0,0 +1,14 @@
+console.log('hola mundo')
+
+function suma (num1,num2){
+    return num1 + num2;
+}
+console.log(suma( 9 , 2));
+
+const mult = (num) => {
+
+    for(let i = 1; i <=num; i++) {
+        console.log(` 9 x ${i}= ${i * 9}`);
+    }
+}
+mult(10);
